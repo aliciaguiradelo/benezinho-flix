201aeb3f1fe1df392bb7c133af2442905c0510b4
Logon PF Fiap <logonpflocal@fiap.com.br>
2023-11-01 19:27:44 -0300

    Initial commit

---
+++ src/main/java/br/com/fiap/Main.java
@@ -0,0 +1,11 @@
+package br.com.fiap;
+
+public class Main {
+
+    private final static String TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkNGRkZDRlMmFiZDM4Mjk1ZjgxNmIxMTZjZTFmMTFhZSIsInN1YiI6IjY1MmVmZWQzMzU4ZGE3NWI1ZjdhYmJlZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.1_tDWTQhkVYUIp9g15o3fgQC4cCJ3bFUc9y1MW9MEyY";
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+    }
+}
